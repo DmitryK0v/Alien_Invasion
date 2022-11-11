393a12d07796e8d3bc5db990cde67d17f80d141c
dimus <dislakoval@gmail.com>
2022-11-11 15:56:36 +0200

    Add Ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,12 +1,18 @@
 import sys
 import pygame
+from setting import Setting
+from ship import Ship
 
 
 def run_game():
     # Инициализирует игру и создает объект экрана.
+    ai_setting = Setting()
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    screen = pygame.display.set_mode(ai_setting.display_resolution)
     pygame.display.set_caption("Alien Invasion")
+    # Создание корабля.
+    starship = Ship(screen)
+
 
     # Запуск основного цикла игры
     while True:
@@ -14,6 +20,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            # При каждом проходе цикла перерисовывается экран.
+            screen.fill(ai_setting.background_color)
+            starship.blitme()
         # Отображение последнего прорисованного экрана.
         pygame.display.flip()
 
---
+++ setting.py
@@ -0,0 +1,11 @@
+"""Класс для хранения всех настроек игры Alien Invasion."""
+
+
+class Setting(object):
+    def __init__(self):
+        self.display_resolution = (1200, 600)
+        self.background_color = (230, 230, 230)
+
+
+
+# Назначение цвета фона.
