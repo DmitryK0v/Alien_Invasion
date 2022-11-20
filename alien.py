491f375b15fef1df46525e6c632ec001da0decd3
dimus <dislakoval@gmail.com>
2022-11-20 15:44:53 +0200

    Refactor; Add game_stats.py file;

--- alien.py
+++ alien.py
@@ -3,31 +3,39 @@ from pygame.sprite import Sprite
 
 
 class Alien(Sprite):
-    """A class representing a single alien"""
+    """A class to represent a single alien in the fleet."""
 
-    def __init__(self, screen, ai_settings):
-        """Initializes the alien and sets its initial position"""
-        super().__init__()
+    def __init__(self, ai_settings, screen):
+        """Initialize the alien, and set its starting position."""
+        super(Alien, self).__init__()
         self.screen = screen
-        self.ai = ai_settings
+        self.ai_settings = ai_settings
 
-        # Load the alien image and assign the rect attribute.
+        # Load the alien image, and set its rect attribute.
         self.image = pygame.image.load('data/images/Alien-1.png')
         self.rect = self.image.get_rect()
 
-        # Each new alien appears in the upper left corner of the screen.
+        # Start each new alien near the top left of the screen.
         self.rect.x = self.rect.width
         self.rect.y = self.rect.height
 
-        # Save the exact position of the alien
+        # Store the alien's exact position.
         self.x = float(self.rect.x)
-        self.alien_speed_factor = 1
+
+    def check_edges(self):
+        """Return True if alien is at edge of screen."""
+        screen_rect = self.screen.get_rect()
+        if self.rect.right >= screen_rect.right:
+            return True
+        elif self.rect.left <= 0:
+            return True
 
     def update(self):
-        """Moving alien to the right"""
-        self.x += self.alien_speed_factor
+        """Move the alien right or left."""
+        self.x += (self.ai_settings.alien_speed_factor *
+                   self.ai_settings.fleet_direction)
         self.rect.x = self.x
 
     def blitme(self):
-        """Displays the alien in its current position."""
+        """Draw the alien at its current location."""
         self.screen.blit(self.image, self.rect)
--- alien_invasion.py
+++ alien_invasion.py
@@ -1,33 +1,44 @@
-# The only file, that must be run.
 import pygame
 from pygame.sprite import Group
 
-from setting import Setting
+from settings import Settings
+from game_stats import GameStats
 from ship import Ship
-import gamefunctions as gf
+import game_functions as gf
 
 
 def run_game():
-    # Initializes the game and creates a screen object.
-    ai_settings = Setting()
+    # Initialize pygame, settings, and screen object.
     pygame.init()
-    screen = pygame.display.set_mode((ai_settings.display_width, ai_settings.display_height))
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
-    # Create ship.
-    starship = Ship(screen, ai_settings)
-    # Create a groups of bullets and Aliens.
-    aliens = Group()
+
+    # Create an instance to store game statistics.
+    stats = GameStats(ai_settings)
+
+    # Set the background color.
+    bg_color = (230, 230, 230)
+
+    # Make a ship, a group of bullets, and a group of aliens.
+    ship = Ship(ai_settings, screen)
     bullets = Group()
-    # Generate of an alien fleet
-    gf.generate_fleet(ai_settings, screen, aliens)
-    # Runs main cycle of game.
+    aliens = Group()
+
+    # Create the fleet of aliens.
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+
+    # Start the main loop for the game.
     while True:
-        gf.check_events(ai_settings, screen, starship, bullets)
-        starship.update()
-        bullets.update()
-        gf.update_aliens(aliens)
-        gf.remove_bullets(bullets)
-        gf.update_screen(ai_settings, screen, starship, aliens, bullets)
+        gf.check_events(ai_settings, screen, ship, bullets)
+
+        if stats.game_active:
+            ship.update()
+            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
+
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
 
 
 run_game()
